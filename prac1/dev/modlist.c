b92a5beb54b00600bc2fcc60dddcf2d8fa83b95e
Borja Lorente <blorente@ucm.es>
2016-10-13 19:42:34 +0200

    Add barebones module

--- prac1/dev/modlist.c
+++ prac1/dev/modlist.c
@@ -3,19 +3,16 @@
 #include <linux/proc_fs.h>
 #include <linux/string.h>
 #include <linux/vmalloc.h>
-<<<<<<< HEAD
-
-MODULE_LICENSE("GPL");
-
-static struct proc_dir_entry *proc_entry;
-=======
 #include <asm-generic/uaccess.h>
 
 MODULE_LICENSE("GPL");
 
-#define BUFFER_LENGTH       PAGE_SIZE
+#define LIST_LENGTH 10
+static int data[LIST_LENGTH];
 
->>>>>>> Initial COmmit
+#define MAX_CHAR 100
+static char *kbuf;
+
 struct list_head storage; /* Lista enlazada */
 
 /* Nodos de la lista */
@@ -24,61 +21,68 @@ typedef struct {
   struct list_head storage_links;
 } list_item_t;
 
-<<<<<<< HEAD
-
-static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
-  printk(KERN_INFO "Modlist: Reading\n");
-  return 0;
-=======
 static struct proc_dir_entry *proc_entry;
 
 static ssize_t modlist_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
   printk(KERN_INFO "Modlist: Reading\n");
-  return len;
->>>>>>> Initial COmmit
+  return 0;
 }
 
 static ssize_t modlist_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
-  printk(KERN_INFO "Modlist: Module unloaded.\n");
-<<<<<<< HEAD
+
+  if ((*off) > 0)
+      return 0;
+
+  if (len + 1 > MAX_CHAR) {
+    printk(KERN_INFO "Modlist: Panic! (not enough space)\n");
+    return -ENOSPC;
+  }
+
+  printk(KERN_INFO "Modlist: Writing.\n");
+  if (copy_from_user( &kbuf[0], buf, len )) {
+    printk(KERN_INFO "Modlist: Panic! (failed copy_from_user)\n");
+    return -EFAULT;
+  }
+  kbuf[len] = '\0'; // Add string terminator
+
+  printk(KERN_INFO "Modlist: Writing %s", kbuf);
+
   return len;
-=======
-  return 0;
->>>>>>> Initial COmmit
 }
 
 static const struct file_operations proc_entry_fops = {
     .read = modlist_read,
-    .write = modlist_write,    
+    .write = modlist_write,
 };
-
-
 
 int init_modlist_module( void ) {
   int ret = 0;
 
-  proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
-  if (proc_entry == NULL) {
+  kbuf = (char *)vmalloc( MAX_CHAR );
+
+  if (!kbuf) {
+    printk(KERN_INFO "Modlist: Panic! (failed vmalloc)\n");
     ret = -ENOMEM;
-    printk(KERN_INFO "Modlist: Can't create /proc entry\n");
   } else {
-    printk(KERN_INFO "Modlist: Module loaded\n");
-  }  
+
+    memset( kbuf, 0, MAX_CHAR );
+
+    proc_entry = proc_create( "modlist", 0666, NULL, &proc_entry_fops);
+    if (proc_entry == NULL) {
+      ret = -ENOMEM;
+      printk(KERN_INFO "Modlist: Can't create /proc entry\n");
+    } else {
+      printk(KERN_INFO "Modlist: Module loaded\n");
+    }    
+  }
 
   return ret;
-<<<<<<< HEAD
-}
-
-
-void exit_modlist_module( void ) {
-=======
-
 }
 
 
 void exit_modlist_module( void )
 {
->>>>>>> Initial COmmit
+  vfree(kbuf);
   remove_proc_entry("modlist", NULL);
   printk(KERN_INFO "Modlist: Module unloaded.\n");
 }
--- prac1/dev/test.sh
+++ prac1/dev/test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+make
+sudo insmod modlist.ko
 echo "Hello 21" > /proc/modlist
 cat /proc/modlist
 echo "Hello 3" > /proc/modlist
 echo "Hello 1" > /proc/modlist
 cat /proc/modlist
+make clean
 sudo rmmod modlist
 sudo dmesg | grep Modlist
